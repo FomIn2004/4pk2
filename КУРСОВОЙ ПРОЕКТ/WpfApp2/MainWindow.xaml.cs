eab4b0f1d08e4a26293a7b575448d545e6beb497
FomIn2004 <91515536+FomIn2004@users.noreply.github.com>
2023-11-25 21:11:12 +0500

    добавлена открывашка и сохраняшка файлов, не бесит

--- КУРСОВОЙ ПРОЕКТ/WpfApp2/MainWindow.xaml.cs
+++ КУРСОВОЙ ПРОЕКТ/WpfApp2/MainWindow.xaml.cs
@@ -15,6 +15,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using Microsoft.Win32;
+using Microsoft.Win32;
+using System;
+using System.IO;
 
 namespace WpfApp2
 {
@@ -76,8 +80,63 @@ namespace WpfApp2
                 ChangeFontSize(selectedSize);
             }
         }
+        private void SaveToFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; // Фильтр для выбора файлов
 
-       
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string filePath = saveFileDialog.FileName;
+
+                    TextRange textRange = new TextRange(NoteContent.Document.ContentStart, NoteContent.Document.ContentEnd);
+                    using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        textRange.Save(fileStream, DataFormats.Text);
+                    }
+
+                    MessageBox.Show("Файл успешно сохранен!", "Сохранение", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка сохранения файла: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; // Фильтр для выбора файлов
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    if (File.Exists(filePath))
+                    {
+                        string fileContent = File.ReadAllText(filePath);
+                        NoteContent.Document.Blocks.Clear();
+                        NoteContent.AppendText(fileContent); // NoteContent - ваш RichTextBox или другой элемент для отображения содержимого файла
+
+                        MessageBox.Show("Файл успешно открыт!", "Открытие файла", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выбранный файл не существует!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка открытия файла: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+
         private void ChangeFontButton_Click(object sender, RoutedEventArgs e)
         {
             string selectedFont = "Arial"; 
